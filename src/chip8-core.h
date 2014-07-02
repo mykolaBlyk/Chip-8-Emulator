88e3401c22ebd0601873f08112cc415b426911aa
blyk.mykola <mykola.blyk@gmail.com>
2014-07-02 03:11:51 +0300

    realise opcode function

--- src/chip8-core.h
+++ src/chip8-core.h
@@ -13,49 +13,73 @@
 class ChipCore
 {
 private:
-        unsigned short opcode;          // двухбайтовый опкод
-        unsigned char V[16];            // регистры общего назначения и флаг переноса VF
-        unsigned short I;               // адрессный регистр
-        unsigned short PC;              // указатель кода
-        unsigned char SP;               // указаель стека
-
-        unsigned short stack[16];       // массив хранящий в себе стек
-
-        unsigned char memory[4096];     // память, в нее загружаем игру
-
-        unsigned char delay_timer;      // таймеры задержки
-        unsigned char sound_timer;      // и звука
-
-        // функция рисующая спрайт в массив screen
+        unsigned short opcode;
+        unsigned char V[16];
+        unsigned short I;
+        unsigned short PC;
+        unsigned char SP;
+        unsigned short stack[16];
+        unsigned char memory[4096];
+        unsigned char delay_timer;
+        unsigned char sound_timer;
         void drawSprite(unsigned char X, unsigned char Y, unsigned char N);
+        unsigned char hp48_flags[8];
 
-        unsigned char hp48_flags[8];    // флаги, необходимы для опкодов Fx75, Fx85
+        void do00EE();
+        void do00Cn();
+        void do00E0();
+        void do1nnn();
+        void do2nnn();
+        void do3xkk();
+        void do4xkk();
+        void do5xy0();
+        void do6xkk();
+        void do7xkk();
+        void do8xy0();
+        void do8xy1();
+        void do8xy2();
+        void do8xy3();
+        void do8xy4();
+        void do8xy5();
+        void do8xy6();
+        void do8xy7();
+        void do8xyE();
+        void do9xy0();
+        void doAnnn();
+        void doBnnn();
+        void doCxkk();
+        void doEx9E();
+        void doExA1();
+        void doFx07();
+        void doFx0A();
+        void doFx15();
+        void doFx18();
+        void doFx1E();
+        void doFx29();
+        void doFx30();
+        void doFx33();
+        void doFx55();
+        void doFx65();
+        void doFx75();
+        void doFx85();
+        void do00FB();
+        void do00FC();
+        void do00FD();
+        void do00FE();
+        void do00FF();
+
+        void drawSpriteChip8Mode(char X, unsigned char Y, unsigned char N);
+        void drawSpriteSCHIPMode(char X, unsigned char Y, unsigned char N);
 
 public:
-        int mode;                       // переменная определяющая текущий режим:
-                                        // 0 - CHIP-8
-                                        // 1 - SCHIP
-
-        unsigned char screen[128][64];  // массив представляющий экран
-
-        unsigned char key[16];          // массив содержащий состояние клавиатуры:
-                                        // 0 - клавиша отпущена
-                                        // 1 - клавиша нажата
-
-        bool stop;                      // переменная для опкода 00FD
-
+        int mode;
+        unsigned char screen[128][64];
+        unsigned char key[16];
+        bool stop;
         ChipCore();
-
-        // инициализация переменных, загрузка шрифтов
         void init();
-
-        // функция загрузки игры в память
         bool loadGame(const char *filename);
-
-        // функция выполняющая опкод
         void executeNextOpcode();
-
-        // функция уменьшающая значения таймеров
         void decreaseTimers();
 };
 #endif
